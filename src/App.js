09fc0fc0ee8dadd48da33464754f0e51fa561008
Josh Chappelow <jdchappelow@gmail.com>
2022-10-21 14:19:55 +0900

    feat: date formatting

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     { title: "Health Insurnace", amount: 400, date: new Date(2022, 10, 20) },
-    { title: "Groceries", amount: 210.5, date: new Date(2022, 10, 20) },
-    { title: "Car Maintenance", amount: 180.13, date: new Date(2022, 10, 20) },
-    { title: "School Supplies", amount: 38.99, date: new Date(2022, 10, 20) },
+    { title: "Groceries", amount: 210.5, date: new Date(2022, 9, 10) },
+    { title: "Car Maintenance", amount: 180.13, date: new Date(2022, 8, 12) },
+    { title: "School Supplies", amount: 38.99, date: new Date(2022, 9, 26) },
   ];
   return (
     <div>
