870025753de809cbdef54d4c8160ff1398bfdc6b
Josh Chappelow <jdchappelow@gmail.com>
2022-10-23 21:37:46 +0900

    feat: controlled controlled component filter added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./NewExpense/ExpensesFilter";
 import Card from "./UI/Card";
 import './Expenses.css'
 
+
+
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  function filterChangeHandler(selectedYear) {
+    setFilteredYear(selectedYear)
+  }
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
